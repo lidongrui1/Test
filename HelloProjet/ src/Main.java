1abf5ed04f18f8eb705d84c1f6e049baf0e33605
jik110110 <1003919376@qq.com>
2019-02-20 23:05:59 +0800

    操他

--- HelloProjet/ src/Main.java
+++ HelloProjet/ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         System.out.println("222HelloWorld!");
         System.out.println("点草小河虾");
         for (int i = 0; i < 10; i++) {
-            System.out.println("点草杨昆");
+            System.out.println("点草东瑞");
         }
-        System.out.println("单槽杨昆");
+        System.out.println("狂日李东瑞");
     }
 }
