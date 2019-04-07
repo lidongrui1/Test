544d81b8910336b8a17db7a9cf452a830bf2dbee
lidongrui <12345678@QQ.com>
2019-04-07 17:59:42 +0800

    AA

--- HelloProjet/ src/Main.java
+++ HelloProjet/ src/Main.java
@@ -6,10 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("hehehi");
         System.out.println("222HelloWorld!");
-        System.out.println("点草小河虾");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("点草东瑞");
-        }
-        System.out.println("狂日李东瑞");
+        
     }
 }
